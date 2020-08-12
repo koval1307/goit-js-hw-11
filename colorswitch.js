c6a44dd69f85d1da8944856ac131809986cfa8c2
Koval <dmytrokovalskyi@yahoo.com>
2020-08-12 21:16:25 +0300

    таймер

--- colorswitch.js
+++ colorswitch.js
@@ -1,35 +1,35 @@
 
-const body = document.querySelector ('body');
-const startBtnRef = document.querySelector('[data-action=start]');
-const stopBtnRef = document.querySelector('[data-action=stop]');
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  let intervalId = null;
-  function disableStartBtn (){
-    startBtn.setAttribute("disabled", "disabled")
-  };
-  function enableStartBtn (){
-    startBtn.removeAttribute("disabled")
-  }
-  function startSwitchColors () {
-      intervalId = setInterval(() => {
-const colorId =randomIntegerFromInterval(0,colors.length);
-body.style.backgroundColor = colors[colorId];
-disableStartBtn
-      }, 1000);
-  }
-  function stopSwitchColors () {
-  clearInterval(intervalId);
-  enableStartBtn
-  }
-  startBtn.addEventListener('click', startSwitchColors);
-  stopBtn.addEventListener('click', stopSwitchColors);
+// const body = document.querySelector ('body');
+// const startBtnRef = document.querySelector('[data-action=start]');
+// const stopBtnRef = document.querySelector('[data-action=stop]');
+// const colors = [
+//     '#FFFFFF',
+//     '#2196F3',
+//     '#4CAF50',
+//     '#FF9800',
+//     '#009688',
+//     '#795548',
+//   ];
+//   const randomIntegerFromInterval = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   };
+//   let intervalId = null;
+//   function disableStartBtn (){
+//     startBtn.setAttribute("disabled", "disabled")
+//   };
+//   function enableStartBtn (){
+//     startBtn.removeAttribute("disabled")
+//   }
+//   function startSwitchColors () {
+//       intervalId = setInterval(() => {
+// const colorId =randomIntegerFromInterval(0,colors.length);
+// body.style.backgroundColor = colors[colorId];
+// disableStartBtn
+//       }, 1000);
+//   }
+//   function stopSwitchColors () {
+//   clearInterval(intervalId);
+//   enableStartBtn
+//   }
+//   startBtn.addEventListener('click', startSwitchColors);
+//   stopBtn.addEventListener('click', stopSwitchColors);
