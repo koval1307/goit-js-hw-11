fab5874f90e5a1afe4af7a41be78354c628c7a63
Koval <dmytrokovalskyi@yahoo.com>
2020-08-20 12:18:24 +0300

    правки в таймере

--- timer.js
+++ timer.js
@@ -1,32 +1,34 @@
 
-// const  daysRef = document.querySelector("[data-value=days]");
-// const  hoursRef = document.querySelector("[data-value=hours]");
-// const  minsRef = document.querySelector("[data-value=mins]");
-// const  secondsRef = document.querySelector("[data-value=secs]");
+const  daysRef = document.querySelector("[data-value=days]");
+const  hoursRef = document.querySelector("[data-value=hours]");
+const  minsRef = document.querySelector("[data-value=mins]");
+const  secondsRef = document.querySelector("[data-value=secs]");
 
-// const timer = {
-//     start(){
-//         const targetDate = new Date('Jun 27, 2021')
-//         setInterval(() => {
-//             const currentTime = Date.now();
-//             const deltaTime = targetDate - currentTime;
-//             updateClockFace(deltaTime)
-//         }, 1000);
-//     }
-// };
-// timer.start();
-//  function updateClockFace(time){
-// const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-// const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-// const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-// const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-// daysRef.textContent = days;
-// hoursRef.textContent = hours;
-// minsRef.textContent = mins;
-// secondsRef.textContent = secs;
-//  }
-//  function pad(value){
-//       return String(value).padStart(2, '0');
-//  }
-
-//  timer.start();
+const timer = {
+    start(){
+        const targetDate = new Date('Jun 27, 2021')
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = targetDate - currentTime;
+            updateClockFace(deltaTime)
+        }, 1000);
+    }
+};
+timer.start();
+ function updateClockFace(time){
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+markupTimer(days,hours,mins,secs)
+ }
+ function pad(value){
+      return String(value).padStart(2, '0');
+ }
+function markupTimer(days,hours,mins,secs) {
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minsRef.textContent = mins;
+    secondsRef.textContent = secs;
+}
+ timer.start();
